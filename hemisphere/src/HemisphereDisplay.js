fc91873d51a18171e327f61884b90edf67ac7531
neelufar0803 <59116102+neelufar0803@users.noreply.github.com>
2021-05-10 15:53:15 +0530

    Add files via upload

---
+++ hemisphere/src/HemisphereDisplay.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Hemisphere.css'
+import northernPic from './images/hemi.jpg';  
+import southernPic from './images/hemi1.jpg';  
+
+  const hemisphereConfig = {
+
+  	Northern:{
+      text: 'it is northern hemisphere',
+      picture: northernPic
+  	},
+  	Southern:{
+
+ text: 'it is southern hemisphere',
+      picture: southernPic
+  	}
+  }
+
+
+ const HemisphereDisplay = ({latitude}) => {
+  const hemisphere = latitude > 0 ? 'Northern' : 'Southern' ;
+  const {text,picture} = hemisphereConfig[hemisphere]
+  return(
+   <div className={hemisphere}>
+     <div className='ui raised text container segment'>
+       <div className='image'>
+       <img src={picture} alt="hemisphere picture"/>
+       </div>
+       <div className='ui teal bottom attached label'>
+          <h1> {text} </h1>
+       </div>
+     </div>
+  </div>
+  )
+
+ }
+
+ export default HemisphereDisplay;
